20c25c3c2bdb2a7a3e2f077172b9ef3ce08d859c
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-03-17 09:15:43 +0530

    feat: executed non-streaming response

--- client/llm_client.py
+++ client/llm_client.py
@@ -1,3 +1,4 @@
+from typing import Any
 from openai import AsyncOpenAI
 
 class LLMClient:
@@ -7,7 +8,7 @@ class LLMClient:
     def get_client(self)->AsyncOpenAI:
         if self._client is None:
             self._client = AsyncOpenAI(
-                apiKey="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", 
+                api_key="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", 
                 base_url="https://openrouter.ai/api/v1"
             )
         return self._client
@@ -16,3 +17,23 @@ class LLMClient:
         if self._client:
             await self._client.close()
             self._client = None
+
+    async def chat_completion(self, messages: list[dict[str, Any]], stream:bool=True):
+
+        client = self.get_client()
+        kwargs={
+            "model":"nvidia/nemotron-3-nano-30b-a3b:free",
+            "messages":messages,
+            "stream":stream
+        }
+        if stream:
+            await self._stream_response(client, kwargs)
+        else:
+            await self._non_stream_response(client, kwargs)
+
+    async def _stream_response(self):
+        pass
+
+    async def _non_stream_response(self, client:AsyncOpenAI, kwargs: dict[str, Any]):
+        response = await client.chat.completions.create(**kwargs)
+        print(response)
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Helllo")
+from client.llm_client import LLMClient
+import asyncio
+
+async def main():
+    client = LLMClient()
+    messages = [{'role':'user','content':"What's up?"}]
+    await client.chat_completion(messages, False)
+    print("Done")
+
+asyncio.run(main())
+    
